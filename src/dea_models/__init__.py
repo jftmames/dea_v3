6a4f105d9f0292f65605fbe0eef0897e0d6b5a20
Jose Fernández Tamame <jftamames@gmail.com>
2025-06-02 00:39:52 +0200

    Update __init__.py

--- src/dea_models/__init__.py
+++ src/dea_models/__init__.py
@@ -4,3 +4,4 @@
 from .utils import validate_positive_dataframe, check_positive_data, check_zero_negative_data
 from .radial import run_ccr, run_bcc
 from .nonradial import run_sbm, run_radial_distance
+from .mpi import compute_malmquist_phi
