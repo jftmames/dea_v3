557ecd3820ec023fb77da97eca6a9dfe89d97fd9
Jose Fernández Tamame <jftamames@gmail.com>
2025-06-02 00:38:02 +0200

    Update __init__.py

--- src/dea_models/__init__.py
+++ src/dea_models/__init__.py
@@ -1,9 +1,6 @@
 # src/dea_models/__init__.py
 
-"""
-Paquete dea_models: lógica de modelos DEA.
-Por ahora solo exportamos radial y utils; los demás módulos se implementarán luego.
-"""
 
 from .utils import validate_positive_dataframe, check_positive_data, check_zero_negative_data
 from .radial import run_ccr, run_bcc
+from .nonradial import run_sbm, run_radial_distance
