3325bc161f9240865a4eb9758ee8897baadfc5ca
Jose Fernández Tamame <jftamames@gmail.com>
2025-06-02 01:53:23 +0200

    Update __init__.py

--- src/dea_models/__init__.py
+++ src/dea_models/__init__.py
@@ -8,7 +8,6 @@ from .mpi import compute_malmquist_phi
 from .cross_efficiency import compute_cross_efficiency
 from .window_analysis import run_window_dea
 from .stochastic import run_stochastic_dea, bootstrap_efficiencies
-from .visualizations import plot_slack_waterfall
 from .auto_tuner import generate_candidates, evaluate_candidates
 from .visualizations import plot_benchmark_spider
 
